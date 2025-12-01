0c078035503592b655834e9fb5e1737544640d0a
damiangarasz <damian.garasz@gmail.com>
2025-12-01 22:32:19 +0100

    lo

--- app/Edycja/Edycja.tsx
+++ app/Edycja/Edycja.tsx
@@ -1,18 +1,33 @@
 import { Pressable, Text, View } from "react-native";
 import { propEdycja } from "../types.ts";
+import { useState } from "react";
 
 export default function Edycja({ fiszki, setFiszki, fiszkaDoEdycji }: propEdycja) {
+
+  const [switch, setSwitch] = useState(false)
+
+  function dodanieFiszka(){
+    return <> </>
+  }
   return (
     <View>
-      <Pressable>
+      <Pressable onPress={()=>{
+        setSwitch(true)
+      }}>
         <Text>Dodaj nowe</Text>
       </Pressable>
+
+      {dodanieFiszka()}
       {Object.values(fiszki[fiszkaDoEdycji])[0].map((element, index) => {
-        console.log(element);
         const key = Object.keys(element)[0] + index;
         return (
-          <View key={key}>
-            <Text>{}</Text>
+          <View key={key} className="flex flex-row gap-3">
+            <Text>{element.polski}</Text>
+            <Text>{element.angielski}</Text>
+            <Text>{element.konteks}</Text>
+            <Pressable>
+              <Text>Edycja</Text>
+            </Pressable>
           </View>
         );
       })}
