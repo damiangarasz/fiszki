2250143d83a0ab546c7c1895cf5dd115194199ce
damiangarasz <damian.garasz@gmail.com>
2025-12-13 16:45:29 +0100

    lol

--- app/UI/MojeFiszkiEkran.tsx
+++ app/UI/MojeFiszkiEkran.tsx
@@ -1,10 +1,18 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useState } from "react";
 import { Pressable, Text, View } from "react-native";
 import { FiszkiWyswietlanieProp, MojeFiszkiEkranMainProp } from "../types.ts";
-import WyswietlanieKart from "./WyswietlanieKart.tsx";
 
 export default function FiszkiWyswietlanie({ fiszki }: FiszkiWyswietlanieProp) {
   const Stack = createNativeStackNavigator();
+  const [jakiZestawDoWyswietlenia, setJakiZestawDoWyswietlenia] = useState("");
+
+  const dupa = fiszki;
+  //losowanie fiszka
+  useState(() => {
+    console.log(fiszki.lista);
+  });
+  //TODO Tutaj jestem lol
 
   function MojeFiszkiEkranMain({ navigation }: MojeFiszkiEkranMainProp) {
     return (
@@ -21,6 +29,7 @@ export default function FiszkiWyswietlanie({ fiszki }: FiszkiWyswietlanieProp) {
               <View key={index} className="border-b border-gray-400 border-dotted">
                 <Pressable
                   onPress={() => {
+                    setJakiZestawDoWyswietlenia(param.key);
                     navigation.navigate("wyswietlanie");
                   }}
                 >
@@ -30,6 +39,24 @@ export default function FiszkiWyswietlanie({ fiszki }: FiszkiWyswietlanieProp) {
             );
           })
         )}
+      </View>
+    );
+  }
+
+  function WyswietlanieKart() {
+    return (
+      <View className="h-[100%] w-[100%] flex">
+        <Text>{jakiZestawDoWyswietlenia}</Text>
+        <View className="realtive m-auto w-[50vw] h-[60vh]">
+          <View className="absolute w-[100%] h-[100%] shadow-xl bg-lime-600 border rounded-xl">
+            <Text>Dupa</Text>
+            <Text></Text>
+          </View>
+          <View className="absolute w-[100%] h-[100%] shadow-xl bg-lime-500 border rounded-xl">
+            {" "}
+            <Text className="m-auto">Dupa</Text>
+          </View>
+        </View>
       </View>
     );
   }
