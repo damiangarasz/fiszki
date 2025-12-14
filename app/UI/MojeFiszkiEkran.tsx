aae5731c4464d7ebe86d6ecb3aa466bbc2cc86d7
damiangarasz <damian.garasz@gmail.com>
2025-12-14 17:38:43 +0100

    wszystko działa

--- app/UI/MojeFiszkiEkran.tsx
+++ app/UI/MojeFiszkiEkran.tsx
@@ -1,18 +1,23 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Image, Pressable, Text, View } from "react-native";
 import { FiszkiWyswietlanieProp, MojeFiszkiEkranMainProp } from "../types.ts";
 
 export default function FiszkiWyswietlanie({ fiszki }: FiszkiWyswietlanieProp) {
   const Stack = createNativeStackNavigator();
   const [jakiZestawDoWyswietlenia, setJakiZestawDoWyswietlenia] = useState("");
+  const [indexFiszek, setIndexFiszek] = useState(0);
+  const [wybranaFiszka, setWybranaFiszka] = useState({ polski: "", angielski: "", kontekst: "" });
 
-  const dupa = fiszki;
   //losowanie fiszka
-  useState(() => {
-    console.log(fiszki.lista);
-  });
-  //TODO Tutaj jestem lol
+  useEffect(() => {
+    //proste losowanie fiszek bez wagi
+    //TODO do rafabrykacji
+    setWybranaFiszka(() => {
+      const randomNumber = Math.floor(Math.random() * fiszki[indexFiszek].lista.length);
+      return fiszki[indexFiszek].lista[randomNumber];
+    });
+  }, [indexFiszek]);
 
   function MojeFiszkiEkranMain({ navigation }: MojeFiszkiEkranMainProp) {
     return (
@@ -30,6 +35,7 @@ export default function FiszkiWyswietlanie({ fiszki }: FiszkiWyswietlanieProp) {
                 <Pressable
                   onPress={() => {
                     setJakiZestawDoWyswietlenia(param.key);
+                    setIndexFiszek(index);
                     navigation.navigate("wyswietlanie");
                   }}
                 >
@@ -46,16 +52,38 @@ export default function FiszkiWyswietlanie({ fiszki }: FiszkiWyswietlanieProp) {
   function WyswietlanieKart() {
     return (
       <View className="h-[100%] w-[100%] flex">
-        <Text>{jakiZestawDoWyswietlenia}</Text>
-        <View className="realtive m-auto w-[50vw] h-[60vh]">
-          <View className="absolute w-[100%] h-[100%] shadow-xl bg-lime-600 border rounded-xl">
-            <Text>Dupa</Text>
-            <Text></Text>
+        <Text className="m-auto text-2xl">{jakiZestawDoWyswietlenia}</Text>
+        <Pressable className="absolute right-10 top-5">
+          <Image
+            source={require("../../assets/images/opcje.png")}
+            style={{ width: 45, height: 45 }}
+          />
+        </Pressable>
+        <Pressable onPress={() => {}}>
+          <View className="realtive m-auto w-[50vw] h-[60vh]">
+            {/* TODO animacja jest spierdolona */}
+            <View
+              className={`absolute w-[100%] h-[100%] shadow-xl bg-lime-600 border  rounded-xl `}
+            >
+              <Text>{wybranaFiszka?.polski}</Text>
+              <Text>{wybranaFiszka?.angielski}</Text>
+              <Text>{wybranaFiszka?.kontekst}</Text>
+            </View>
+            <View className={`absolute w-[100%] h-[100%] shadow-xl bg-lime-500 border rounded-xl`}>
+              <Text className="m-auto">{wybranaFiszka?.polski}</Text>
+            </View>
           </View>
-          <View className="absolute w-[100%] h-[100%] shadow-xl bg-lime-500 border rounded-xl">
-            {" "}
-            <Text className="m-auto">Dupa</Text>
-          </View>
+        </Pressable>
+        <View className="flex flex-row">
+          <Pressable className="w-[33vw] h-16  bg-green-600">
+            <Text className="m-auto text-5xl">Znam</Text>
+          </Pressable>
+          <Pressable className="w-[33vw] h-16 bg-slate-300">
+            <Text className="m-auto text-center text-2xl">Troche znam, a troche nie znam</Text>
+          </Pressable>
+          <Pressable className="w-[33vw] h-16 bg-red-700">
+            <Text className="m-auto text-5xl">Nie znam</Text>
+          </Pressable>
         </View>
       </View>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+// const {
+//   default: keyframes,
+// } = require("react-native-reanimated/lib/typescript/css/stylesheet/keyframes");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  theme: { extend: {} },
+  plugins: ["react-native-reanimated/plugin"],
 };
