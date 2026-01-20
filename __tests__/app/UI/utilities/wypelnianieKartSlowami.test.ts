bf4a15fb4918ea3f87742ad3d5d4b357bb196e4d
damiangarasz <damian.garasz@gmail.com>
2026-01-20 17:53:02 +0100

    testuje wypelnianieKartSlowami WIP

---
+++ __tests__/app/UI/utilities/wypelnianieKartSlowami.test.ts
@@ -0,0 +1,16 @@
+import { wypelnianieKartSlowami } from "@/app/UI/utilities/logic/wypelnianieKartSlowami";
+
+//TODO tutaj jestem
+describe("testowanie funkcji wypelnianieKartSlowami", () => {
+  it("co jeśli wybranaFiszka nie istnieje", () => {
+    wypelnianieKartSlowami({
+      randomNum,
+      opcjeJezyj,
+      setFront,
+      setBack,
+      wybranaFiszka,
+      setTriggerReload,
+    });
+  });
+  it("");
+});
